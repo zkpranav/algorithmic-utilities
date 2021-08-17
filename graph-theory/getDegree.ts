1a72ef7f4f68d6551cd9a2261b6dd3519bae2425
zkpranav <zkpranav@icloud.com>
2021-08-17 20:30:24 +0530

    Implementing the algorithm to find the degree of a vertex in a graph

---
+++ graph-theory/getDegree.ts
@@ -0,0 +1,16 @@
+type AdjecencyMatrix = number[][]
+
+function getDegree(adjMatrix: AdjecencyMatrix, vertexColumnPosition: number): number {
+    if (!Number.isInteger(vertexColumnPosition) || vertexColumnPosition >= adjMatrix.length || vertexColumnPosition < 0) {
+        return NaN
+    }
+
+    let degree: number = 0
+    adjMatrix.forEach((possibleEdges) => {
+        if (possibleEdges[vertexColumnPosition] > 0) {
+            degree += 1
+        }
+    })
+
+    return degree
+}
